34c3e97d2a10ec2209788ff7ce586701021c0473
Jabberski <chalasiewicz97@gmail.com>
2021-03-11 22:56:02 +0100

    Initial commit

---
+++ src/main/java/pl/cloth/shop/controller/IndexController.java
@@ -0,0 +1,19 @@
+package pl.cloth.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+@Slf4j
+@RequiredArgsConstructor
+public class IndexController {
+
+    @GetMapping("")
+    public String index(){
+        return "/index/index";
+    }
+}
